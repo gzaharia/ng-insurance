ab1be3705714fed289234fb45527b97e446bd8a1
silver <Mihx222@gmail.com>
2019-05-15 12:35:48 +0300

    sync.

--- ng-insurance/src/app/components/admin/user/user.component.ts
+++ ng-insurance/src/app/components/admin/user/user.component.ts
@@ -1,6 +1,7 @@
 import {Component, OnInit} from '@angular/core';
 import {UserService} from '../../../service/user/user.service';
 import {EmployeeViewModel} from '../../../model/employee/employee-view-model';
+import {Router} from '@angular/router';
 
 @Component({
   selector: 'app-user',
@@ -10,7 +11,10 @@ import {EmployeeViewModel} from '../../../model/employee/employee-view-model';
 export class UserComponent implements OnInit {
   employees: EmployeeViewModel[];
 
-  constructor(private userService: UserService) {
+  constructor(
+    private userService: UserService,
+    private router: Router
+  ) {
     this.getAllEmployees();
   }
 
