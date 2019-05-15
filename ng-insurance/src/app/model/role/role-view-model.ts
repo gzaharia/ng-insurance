ed11e807d3ca87444ff4f663681eb35e099e2bb3
silver <Mihx222@gmail.com>
2019-05-15 12:17:49 +0300

    Finished profile.

--- ng-insurance/src/app/model/role/role-view-model.ts
+++ ng-insurance/src/app/model/role/role-view-model.ts
@@ -1,3 +1,4 @@
 export interface RoleViewModel {
   name: string;
+  checked?: boolean;
 }
---
+++ ng-insurance/src/app/service/role/role.service.ts
@@ -0,0 +1,18 @@
+import { Injectable } from '@angular/core';
+import {ApiService} from '../shared/api.service';
+import {HttpClient} from '@angular/common/http';
+import {Observable} from 'rxjs';
+import {RoleViewModel} from '../../model/role/role-view-model';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class RoleService {
+  private GET_ALL_ROLES_URL = `${ApiService.BASE_URL}\\admin\\roles`;
+
+  constructor(private http: HttpClient) {}
+
+  getAllRoles(): Observable<RoleViewModel[]> {
+    return this.http.get<RoleViewModel[]>(this.GET_ALL_ROLES_URL);
+  }
+}
