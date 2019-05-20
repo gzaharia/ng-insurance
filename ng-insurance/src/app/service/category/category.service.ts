dba14c9be8434d8cba3211490febce661c43eeb2
was30 <gurduzavasile@gmail.com>
2019-05-20 12:43:27 +0300

    added order view model

--- ng-insurance/src/app/service/category/category.service.ts
+++ ng-insurance/src/app/service/category/category.service.ts
@@ -29,7 +29,7 @@ export class CategoryService {
     return this.http.get<Category[]>(this.GET_ACTIVE_CATEGORIES);
   }
 
-  getOneCategory(id: number): Observable<Category>{
+  getOneCategory(id: number): Observable<Category> {
     return this.http.get<Category>(this.GET_ONE_CATEGORY + id);
   }
 
