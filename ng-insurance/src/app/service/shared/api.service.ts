af9cab42bbca78c07e25422bcfcde94b0a869a1f
Mihx222 <Mihx222@gmail.com>
2019-05-29 16:46:35 +0300

    config test

--- ng-insurance/src/app/service/shared/api.service.ts
+++ ng-insurance/src/app/service/shared/api.service.ts
@@ -4,7 +4,7 @@ import {Injectable} from '@angular/core';
   providedIn: 'root'
 })
 export class ApiService {
-  static BASE_URL = 'https://insurancerest.herokuapp.com';
+  static BASE_URL = 'https://insurancerest.herokuapp.com/api';
 
   constructor() {
   }
